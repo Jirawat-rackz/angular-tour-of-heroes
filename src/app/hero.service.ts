3c09c741ebd558fb66a10fb8fe20b4767ca7790f
Jirawat-rackz <Jirawat_ku@kkumail.com>
2022-11-02 11:15:36 +0700

    [6.] add service api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,17 +3,23 @@ import { Hero } from 'interfaces/hero';
 import { HEROES } from 'mocks/mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessagesService } from 'app/messages.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessagesService) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessagesService
+  ) {}
+
+  private heroesUrl = 'http://localhost:8080/heroes';
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
     this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
